611f39cc5977a1f7f941c790c4e6fc3f3c36e900
Matthieu Turmo <Matthieu@MacBook-Pro-de-Matthieu.local>
2017-07-02 12:14:28 +0200

    Initial Commit

---
+++ Projet_Maths/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Projet_Maths
+//
+//  Created by Matthieu Turmo on 02/07/2017.
+//  Copyright © 2017 Matthieu Turmo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
